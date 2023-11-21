8ee05c7ff41976d67fa2fa07e20ea42c73775d35
Anton Ovod <antovod36@gmail.com>
2023-11-21 11:07:21 +0100

    Finish work on Quadratic Equation project

--- QuadraticEquation/Program.cs
+++ QuadraticEquation/Program.cs
@@ -1,7 +1,7 @@
 ﻿double a, b, c;
 
 
-Console.WriteLine("QuadraticEquation: ax^2 + bx + c");
+Console.WriteLine("Quadratic Equation: ax^2 + bx + c");
 Console.Write("Input coefficient `a`: ");
 while(true)
 {
@@ -15,13 +15,35 @@ while (true)
 {
     bool valid = double.TryParse(Console.ReadLine(), out b);
     if (valid) break;
-    Console.WriteLine("Please, enter a valid double number `a`.");
+    Console.WriteLine("Please, enter a valid double number `b`.");
 }
 
 Console.Write("Input coefficient `c`: ");
 while (true)
 {
-    bool valid = double.TryParse(Console.ReadLine(), out b);
+    bool valid = double.TryParse(Console.ReadLine(), out c);
     if (valid) break;
-    Console.WriteLine("Please, enter a valid double number `a`.");
+    Console.WriteLine("Please, enter a valid double number `c`.");
+}
+
+double d = Math.Pow(b, 2) - 4 * a * c;
+
+if(d > 0)
+{
+    double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+    double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+
+    Console.WriteLine("D = {0}, hence there are two possible answers " +
+        "x1 = {1} and x2 = {2}", d, x1, x2);
+}
+else if(d == 0)
+{
+    double x = -b / (2 * a);
+    Console.WriteLine("D = 0, hence there is only one possible" +
+        "answer x1 and it's equal to {0}", x);
+}
+else
+{
+    Console.WriteLine("D < 0, hence there are no possible answers for the provided equation.");
+
 }

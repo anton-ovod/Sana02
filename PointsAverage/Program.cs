0a78195ac63b5da67090e1e1f555eafa6d0a4c29
Anton Ovod <antovod36@gmail.com>
2023-11-21 11:18:06 +0100

    Finish work on PointsAverage project

--- PointsAverage/Program.cs
+++ PointsAverage/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+double sumOfPoints = 0;
+
+double numberOfPoints = 5;
+
+for (int i = 0; i < numberOfPoints; i++)
+{
+    double point = 0;
+
+    Console.Write("Input mark number [{0}]: ", i + 1);
+    while(true)
+    {
+        bool valid = double.TryParse(Console.ReadLine(), out point);
+        if (point < 0 || point > 100) valid = false;
+        if (valid) break;
+        Console.WriteLine("Incorrect mark. Try again.");
+    }
+    sumOfPoints += point;
+}
+
+double averagePoint = sumOfPoints / numberOfPoints;
+
+Console.WriteLine(averagePoint >= 50 ? "Average of student's points is greater or equal to 50, hence he/she is allowed to take an exam." :
+    "Average of student's points is less than 50, hence he/she is not allowed to take an exam.");

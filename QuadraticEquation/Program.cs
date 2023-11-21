017a82086b61de7a2ee9e264eb3356fb765cebc5
Anton Ovod <antovod36@gmail.com>
2023-11-21 10:49:08 +0100

    Initialized the solution and created project Quadratic Equation

---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,27 @@
+﻿double a, b, c;
+
+
+Console.WriteLine("QuadraticEquation: ax^2 + bx + c");
+Console.Write("Input coefficient `a`: ");
+while(true)
+{
+    bool valid = double.TryParse(Console.ReadLine(), out a);
+    if (valid) break;
+    Console.WriteLine("Please, enter a valid double number `a`.");
+}
+
+Console.Write("Input coefficient `b`: ");
+while (true)
+{
+    bool valid = double.TryParse(Console.ReadLine(), out b);
+    if (valid) break;
+    Console.WriteLine("Please, enter a valid double number `a`.");
+}
+
+Console.Write("Input coefficient `c`: ");
+while (true)
+{
+    bool valid = double.TryParse(Console.ReadLine(), out b);
+    if (valid) break;
+    Console.WriteLine("Please, enter a valid double number `a`.");
+}
